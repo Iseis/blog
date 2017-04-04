539f5cb3be9204ed31ad11a63bea08a99d1af74d
aowens <aowens@rosenaviation.com>
2017-04-03 17:20:34 -0700

    updates

--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,13 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 
-def post_list(request, test=False):
-    if test:
-        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-        return redirect('post_detail', {'posts': posts})
-    else:
-        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-        return render(request, 'blog/post_list.html', {'posts': posts})
+def post_list(request):
+
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 
 def post_detail(request, pk):
@@ -19,6 +17,7 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
+@login_required
 def post_new(request):
 
     if request.method == "POST":
@@ -33,6 +32,7 @@ def post_new(request):
         return render(request, 'blog/post_edit.html', {'form': form})
 
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -47,11 +47,13 @@ def post_edit(request, pk):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
+@login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 
+@login_required
 def post_publish(request, pk):
 
     post = get_object_or_404(Post, pk=pk)
@@ -59,6 +61,7 @@ def post_publish(request, pk):
     return redirect('post_detail', pk=pk)
 
 
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
